e58591aba6bfc7d6de60469b4345b4aabdf66102
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-09-01 10:46:58 +0300

    fixes in pagination and formatting were added

--- src/PostList.js
+++ src/PostList.js
@@ -1,37 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import 'typeface-roboto';
 import Button from '@material-ui/core/Button';
 
 import './Styles/PostList.css';
-import Pagination from './Pagination';
 import { connect } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
+import Pagination from './Pagination';
 import {
   posts,
   getPostThunkCreator,
   setCurrentPage,
   page,
   perPage,
-        } from './store';
+} from './store';
 
 const API_URL = 'https://simple-blog-api.crew.red/posts';
 
 class PostList extends React.Component {
-
   componentDidMount() {
-    this.props.getPostThunkCreator()
+    this.props.getPostThunkCreator();
   }
 
   handlePageChange = (currentPage) => {
-    this.props.setCurrentPage(currentPage)
+    this.props.setCurrentPage(currentPage);
   };
 
   handleDelete = (id) => {
-    fetch(API_URL + "/" + id, { method: 'DELETE'})
-      .then(() => {console.log('removed')})
-      .catch(err => {console.error(err)})
+    const { posts, page, setCurrentPage } = this.props;
+    fetch(API_URL + '/' + id, { method: 'DELETE' })
+      .then(() => { console.log('removed'); })
+      .catch((err) => { console.error(err); })
       .then(() => this.props.getPostThunkCreator())
+      .then(() => {
+        if (posts.length % 5 === 0) { setCurrentPage(page - 1); }
+        if (posts.length <= 5) { setCurrentPage(0); }
+      });
   }
 
   render() {
@@ -41,36 +46,55 @@ class PostList extends React.Component {
     return (
       <div className="post_list">
         <h1>Post list</h1>
-        <Button><NavLink to="/new_post" >Add the new post</NavLink></Button>
-          {posts.slice(firstPosition, lastPosition).map(post =>
-            <div  key={Math.random()} className="post_item">
-            <Link to={`/posts/${post.id}`}  className="post_item_link">
+        <Button><NavLink to="/new_post">Add the new post</NavLink></Button>
+        {posts.slice(firstPosition, lastPosition).map(post =>
+          <div key={Math.random()} className="post_item">
+            <Link to={`/posts/${post.id}`} className="post_item_link">
               <ul className={post.id ? '' : 'del_buttom_disable'}>
-                <li className="postlist_heading">Title: <b>{post.title}</b></li>
-                <li className="postlist_heading">Author: <b>{post.creator}</b></li>
-                <li className="postlist_heading">Date: {post.date}</li>
+                <li className="postlist_heading">
+                  Title:
+                  <b>
+                    {post.title}
+                  </b>
+                </li>
+                <li className="postlist_heading">
+                  Author:
+                  <b>
+                    {post.creator}
+                  </b>
+                </li>
+                <li className="postlist_heading">
+                  Date:
+                  {post.date}
+                </li>
               </ul>
             </Link>
-              <button
-                onClick={() => {this.handleDelete(post.id)}}
-                className='destroy'
-                disabled={!post.id}
-              >
-              </button>
-            </div>
-            )}
-        {posts.length < 6 ? "" :
-          <Pagination
-            page={page}
-            perPage={perPage}
-            total={posts.length}
-            handlePageChange={this.handlePageChange}
-          />
-        }
+            <button
+              type="button"
+              onClick={() => { this.handleDelete(post.id); }}
+              className='destroy'
+              disabled={!post.id}
+            />
+          </div>)}
+        {posts.length < 6 ? ''
+          : <Pagination
+              page={page}
+              perPage={perPage}
+              total={posts.length}
+              handlePageChange={this.handlePageChange}
+            />}
       </div>
-    )
+    );
   }
 }
+
+PostList.propTypes = {
+  page: PropTypes.number.isRequired,
+  perPage: PropTypes.number.isRequired,
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+  getPostThunkCreator: PropTypes.func.isRequired,
+  setCurrentPage: PropTypes.func.isRequired,
+};
 
 const getData = (state) => ({
   posts: posts(state),
@@ -81,4 +105,4 @@ const getMethod = (dispatch) => ({
   getPostThunkCreator: () => dispatch(getPostThunkCreator()),
   setCurrentPage: (value) => dispatch(setCurrentPage(value)),
 });
-export default connect(getData, getMethod) (PostList);
+export default connect(getData, getMethod)(PostList);
